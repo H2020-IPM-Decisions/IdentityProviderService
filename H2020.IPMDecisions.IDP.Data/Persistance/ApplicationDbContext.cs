25ce60188ba61f4494100ed0e9b62a8392546819
Antonio Calatayud <antonio.calatayud@adas.co.uk>
2020-01-28 15:51:37 +0000

    Add Data project with application db context

---
+++ H2020.IPMDecisions.IDP.Data/Persistance/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace H2020.IPMDecisions.IDP.Data.Persistance
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<IdentityUser>().ToTable("User");
+            builder.Entity<IdentityRole>().ToTable("Role");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaim");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRole");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogin");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaim");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserToken");
+        }
+    }
+}
