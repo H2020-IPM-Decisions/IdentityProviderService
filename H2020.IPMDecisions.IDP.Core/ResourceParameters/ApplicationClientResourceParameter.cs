01efdcf6f80c4013fc40bff9d9c42556997b51dc
Antonio Calatayud <antonio.calatayud@adas.co.uk>
2020-02-07 17:02:34 +0000

    Extract parameter resource

--- H2020.IPMDecisions.IDP.Core/ResourceParameters/ApplicationClientResourceParameter.cs
+++ H2020.IPMDecisions.IDP.Core/ResourceParameters/ApplicationClientResourceParameter.cs
@@ -1,8 +1,28 @@
 namespace H2020.IPMDecisions.IDP.Core.ResourceParameters
 {
-    public class ApplicationUserResourceParameter : BaseResourceParameter
+    public class ApplicationClientResourceParameter : BaseResourceParameter
     {
-        const int maxPageSize = 40;
+        public bool? IsEnabled { get; set; }
+        const int maxPageSize = 20;
         private int _pageSize = 10;
+        public override int PageSize
+        {
+            get { return _pageSize; }
+            set { _pageSize = (value > maxPageSize) ? maxPageSize : value; }
+        }
+        public override string OrderBy
+        {
+            get => base.OrderBy;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    base.OrderBy = "Name";
+                }
+
+                base.OrderBy = value;
+            }
+        }
+
     }
 }
