3a9e6f9f796f688c4c64493eac4a9b3160ad37f3
Antonio Calatayud <antonio.calatayud@adas.co.uk>
2020-02-21 11:38:02 +0000

    Ensure claims have data when removing

--- H2020.IPMDecisions.IDP.Core/Dtos/ClaimDto.cs
+++ H2020.IPMDecisions.IDP.Core/Dtos/ClaimDto.cs
@@ -4,7 +4,10 @@ namespace H2020.IPMDecisions.IDP.Core.Dtos
 {
     public abstract class ClaimDto
     {
+        [Required]
         public virtual string Type { get; set; }
+        
+        [Required]
         public virtual string Value { get; set; }
     }
 }
