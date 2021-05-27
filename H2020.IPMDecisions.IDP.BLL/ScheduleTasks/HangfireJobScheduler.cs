1bc6d3ec99875621d259553e8b20f75177e94806
Antonio Calatayud <antonio.calatayud@adas.co.uk>
2021-05-27 11:41:24 +0200

    Add dates to send email on appstettings

--- H2020.IPMDecisions.IDP.BLL/ScheduleTasks/HangfireJobScheduler.cs
+++ H2020.IPMDecisions.IDP.BLL/ScheduleTasks/HangfireJobScheduler.cs
@@ -1,4 +1,5 @@
 using System;
+using H2020.IPMDecisions.IDP.Core.Configuration;
 using Hangfire;
 using Microsoft.Extensions.Configuration;
 
@@ -8,26 +9,30 @@ namespace H2020.IPMDecisions.IDP.BLL.ScheduleTasks
     {
         public static void HangfireScheduleJobs(IConfiguration configuration)
         {
-            // ToDo Get months from Configuration File
+            var emailConfirmationAllowanceHours = configuration.GetSection(nameof(InactiveUsers)).Get<InactiveUsers>();
+            var firstEmail = emailConfirmationAllowanceHours.FirstEmailMonthInactive;
+            var secondEmail = emailConfirmationAllowanceHours.SecondEmailMonthInactive;
+            var lastEmail = emailConfirmationAllowanceHours.LastEmailMonthInactive;
+            var deleteAccount = emailConfirmationAllowanceHours.DeleteAccountMonthInactive;
 
-            RecurringJob.RemoveIfExists("Send Initial Inactive Email After 6 Months");
-            RecurringJob.AddOrUpdate<MaintenanceJobs>("Send Initial Inactive Email After 6 Months",
-                job => job.ProcessInactiveUser(JobCancellationToken.Null, 6, 0, false),
+            RecurringJob.RemoveIfExists(string.Format("Send Initial Inactive Email After {0} Months", firstEmail));
+            RecurringJob.AddOrUpdate<MaintenanceJobs>(string.Format("Send Initial Inactive Email After {0} Months", firstEmail),
+                job => job.ProcessInactiveUser(JobCancellationToken.Null, firstEmail, 0, false),
                 Cron.Weekly(DayOfWeek.Wednesday, 10), TimeZoneInfo.Utc);
 
-            RecurringJob.RemoveIfExists("Send Second Inactive Email After 10 Months");
-            RecurringJob.AddOrUpdate<MaintenanceJobs>("Send Second Inactive Email After 10 Months",
-                job => job.ProcessInactiveUser(JobCancellationToken.Null, 10, 1, false),
+            RecurringJob.RemoveIfExists(string.Format("Send Second Inactive Email After {0} Months", secondEmail));
+            RecurringJob.AddOrUpdate<MaintenanceJobs>(string.Format("Send Second Inactive Email After {0} Months", secondEmail),
+                job => job.ProcessInactiveUser(JobCancellationToken.Null, secondEmail, 1, false),
                 Cron.Weekly(DayOfWeek.Tuesday, 10), TimeZoneInfo.Utc);
 
-            RecurringJob.RemoveIfExists("Send Last Inactive Email After 11 Months");
-            RecurringJob.AddOrUpdate<MaintenanceJobs>("Send Last Inactive Email After 11 Months",
-                job => job.ProcessInactiveUser(JobCancellationToken.Null, 11, 2, false),
+            RecurringJob.RemoveIfExists(string.Format("Send Last Inactive Email After {0} Months", lastEmail));
+            RecurringJob.AddOrUpdate<MaintenanceJobs>(string.Format("Send Last Inactive Email After {0} Months", lastEmail),
+                job => job.ProcessInactiveUser(JobCancellationToken.Null, lastEmail, 2, false),
                 Cron.Weekly(DayOfWeek.Monday, 10), TimeZoneInfo.Utc);
 
-            RecurringJob.RemoveIfExists("Remove Inactive User After 12 Months");
-            RecurringJob.AddOrUpdate<MaintenanceJobs>("Remove Inactive User After 12 Months",
-                job => job.ProcessInactiveUser(JobCancellationToken.Null, 12, 3, true),
+            RecurringJob.RemoveIfExists(string.Format("Remove Inactive User After {0} Months", deleteAccount));
+            RecurringJob.AddOrUpdate<MaintenanceJobs>(string.Format("Remove Inactive User After {0} Months", deleteAccount),
+                job => job.ProcessInactiveUser(JobCancellationToken.Null, deleteAccount, 3, true),
                 Cron.Weekly(DayOfWeek.Monday, 9), TimeZoneInfo.Utc);
         }
     }
